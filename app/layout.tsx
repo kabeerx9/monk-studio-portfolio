adf49f78ef154d4a38a3a023d0f53ff9e5edce6b
kabeerx9 <kabeer786joshi@gmail.com>
2024-12-19 17:28:30 +0530

    Readme update

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,8 +6,8 @@ import { ThemeProvider } from "./provider";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Akash's Portfolio",
-  description: "Minimal Modern Porfolio website",
+  title: "Monk Studio",
+  description: "Monk Studio",
 };
 
 export default function RootLayout({
